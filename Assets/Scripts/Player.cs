d5c243db875986dd94b7b7298b84848104fc0645
Ryan Stokes <ryan.stokes24@students.bhasvic.ac.uk>
2025-09-29 13:59:40 +0100

    Added part one of the project
    Added moving, jumping, animations
    Dashing is WIP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,91 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
+    private float moveX;
+    [SerializeField]
+    private float move = 5f, jump = 10f;
+    private bool grounded;
+    private bool canDash;
+
+    private Rigidbody2D body;
+    private BoxCollider2D box;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        body = GetComponent<Rigidbody2D>();
+        box = GetComponent<BoxCollider2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
     void Update()
     {
-        print(1 / Time.deltaTime);
+        Move();
+        Jump();
+        Animate();
+        Dash();
+    }
+
+    private void Move()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveX, 0, 0) * move * Time.deltaTime;
+    }
+
+    private void Jump()
+    {
+        if (grounded && Input.GetButtonDown("Jump"))
+        {
+            body.AddForce(new Vector2(0, jump), ForceMode2D.Impulse);
+            grounded = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.CompareTag("floor"))
+        {
+            grounded = true;
+        }
+    }
+
+    private void Animate()
+    {
+        if(moveX > 0)
+        {
+            anim.SetBool("isRunning", true);
+            sprite.flipX = false;
+        }
+        else if (moveX < 0)
+        {
+            anim.SetBool("isRunning", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+    }
+
+    private void Dash()
+    {
+        if(Input.GetKey(KeyCode.LeftShift) && canDash)
+        {
+            Debug.Log("input");
+            body.AddForce(new Vector2(10, 0), ForceMode2D.Impulse);
+            canDash = false;
+            Cooldown();
+        }
+    }
+
+    private IEnumerator Cooldown()
+    {
+        yield return new WaitForSeconds(2);
+        canDash = True;
     }
 }
