72387fc4cd37030187dcb1c8b8a82988a3b34e28
Ryan Stokes <ryan.stokes24@students.bhasvic.ac.uk>
2025-10-08 10:01:02 +0100

    Added parts of merchant code

--- Assets/Scripts/Merchant.cs
+++ Assets/Scripts/Merchant.cs
@@ -1,16 +1,26 @@
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class Merchant : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private string[] possibleItems = new string[] { "Ace", "Monter", "Broken Sword", "Gun" }, inventory;
+    private int[] prices;
+    private int index;
+    [SerializeField]
+    private GameObject[] itemRef;
+    private GameObject item;
+
     void Start()
     {
-        
+        for (int i = 0; i < 3; i++)
+        {
+            prices[i] = Random.Range(50, 200);
+            inventory[i] = possibleItems[Random.Range(0, possibleItems.Length)];
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MakeItem()
     {
-        
+        index = Random.Range(0, itemRef.length);
     }
 }
